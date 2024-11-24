351e496441de8b4f90aa433c9c78867202332d18
emkay3002 <emankhalid2003@gmail.com>
2024-11-24 19:42:45 +0500

    login/signup backend complete

--- routes/ProductRouter.js
+++ routes/ProductRouter.js
@@ -3,6 +3,7 @@ const { ensureAuthenticated } = require("../middlewares/Auth");
 const router = require("express").Router();
 
 router.get("/", ensureAuthenticated, (req, res) => {
+  console.log("----- logged in user details -----", req.user);
   res.status(200).json([
     {
       name: "Avengers Endgame",
