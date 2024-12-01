ce23d830ac4390f437535bb426fd1ab6899a5701
emkay3002 <emankhalid2003@gmail.com>
2024-12-02 01:06:53 +0500

    home page and logout

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,33 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { handleSuccess } from "../utils";
 function Home() {
-  return <div>Home</div>;
+  const [loggedInUser, setLoggedInUser] = useState("");
+  const navigate = useNavigate();
+  useEffect(() => {
+    setLoggedInUser(localStorage.getItem("loggedInUser"));
+  }, []);
+
+  const handleLogout = (e) => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("loggedInUser");
+    handleSuccess("Logout successful");
+    setTimeout(() => {
+      navigate("/login");
+    }, 1000);
+    //window.location.href = "/login";
+  };
+
+  return (
+    <div>
+      <h1>{loggedInUser}</h1>
+      <button onClick={handleLogout}>Logout</button>
+      <>
+        <ToastContainer />
+      </>
+    </div>
+  );
 }
 
 export default Home;
