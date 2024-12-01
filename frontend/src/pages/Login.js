dc80e05dd65337ccd5e68f8c5ab77a9f66ebcc5c
emkay3002 <emankhalid2003@gmail.com>
2024-12-01 21:14:44 +0500

    login with mock fetch

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,117 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { handleError, handleSuccess } from "../utils";
 function Login() {
-  return <div>Login</div>;
+  const [loginInfo, setLoginInfo] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    console.log(name, value);
+    const copyLoginInfo = { ...loginInfo };
+    copyLoginInfo[name] = value;
+    setLoginInfo(copyLoginInfo);
+  };
+  const mockFetch = (url, options) => {
+    return new Promise((resolve, reject) => {
+      if (
+        url === "http://localhost:3000/auth/login" &&
+        options.method === "POST"
+      ) {
+        resolve({
+          json: () =>
+            Promise.resolve({
+              message: "Login successful",
+              success: true,
+              jwtToken: "token",
+              name: "name",
+            }),
+        });
+      } else {
+        reject(new Error("Not Found"));
+      }
+    });
+  };
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const { email, password } = loginInfo;
+    //frontend validation
+    if (!email || !password) {
+      return handleError("All fields are required");
+    }
+    if (password.length < 4) {
+      return handleError("Password must be at least 4 characters long");
+    }
+    try {
+      const url = "http://localhost:3000/auth/login";
+      console.log(loginInfo);
+      const response = await mockFetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginInfo),
+      });
+      const result = await response.json();
+      console.log("API Response:", result);
+
+      const { success, message, jwtToken, name, error } = result;
+      if (success) {
+        handleSuccess(message);
+        localStorage.setItem("token", jwtToken);
+        localStorage.setItem("loggedInUser", name);
+        setTimeout(() => {
+          navigate("/home");
+        }, 1000);
+      } else if (error) {
+        const details = error?.details[0]?.message;
+        handleError(details);
+      } else if (!success) {
+        handleError(message);
+      }
+      console.log(result);
+    } catch (err) {
+      handleError(err);
+    }
+  };
+  return (
+    <div className="container">
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            onChange={handleChange}
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            value={loginInfo.email}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            onChange={handleChange}
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+            value={loginInfo.password}
+          />
+        </div>
+        <button type="submit">Login</button>
+        <span>
+          Don't have an account?
+          <Link to="/signup">Signup</Link>
+        </span>
+      </form>
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default Login;
